8f524c067fed207d2bb61db2db3ac9a39ca7b5b1
nelitonps <neliton.net@hotmail.com>
2023-06-22 16:03:43 -0300

    Tarefa modulo 9

---
+++ src/br/com/npsouza/Principal.java
@@ -0,0 +1,12 @@
+package br.com.npsouza;
+
+public class Principal {
+    public static void main(String[] args){
+        int valor1 = 10;
+        System.out.println("Valor1: " + valor1);
+
+        Integer valor2 = valor1 + 2;
+        System.out.println("Valor2: " + valor2);
+
+    }
+}
